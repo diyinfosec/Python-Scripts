c602aa12d1395aca7c0a2da664305d7d6cbffefa
diyinfosec <diyinfosec@gmail.com>
2020-06-18 16:02:14 +0100

    Update boto3_download_from_s3.py

--- boto3_download_from_s3.py
+++ boto3_download_from_s3.py
@@ -22,24 +22,24 @@ local_file_path='/users/myuser/dest_dir/'
 
 #- Tell AWS about wanting to access the s3 bucket:
 #- 1. Create a session object using the Access and Secret Keys
-#- 2. Create a s3 object by linking the session object to the s3 resource
-#- 3. Create a bucket object from the s3 object and specify the name of the bucket
-#- Essentially we are doing: Session_Object --> S3_Resource_Object --> Bucket_Object
+#- 2. Create a resource object by linking the session object to the s3 resource
+#- 3. Create a s3 bucket object from the resource object
+#- Essentially we are doing: Session_Object --> Resource_Object --> Bucket_Object
 session_object = Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-s3_object = session_object.resource('s3')
-bucket_object = s3_object.Bucket(bucket_name)
+resource_object = session_object.resource('s3')
+bucket_object = resource_object.Bucket(bucket_name)
 
 #- Enumerate the files inside the s3 bucket
-for s3_file in bucket_object.objects.all():
+for file_objects in bucket_object.objects.all():
 		#- Get the s3 file name
-		s3_file_name=s3_file.key
-		#print(s3_file_name)
+		file_name=file_objects.key
+		#print(file_name)
 
 		#- Give a local file name that you want for the downloaded file
-		local_file_name=local_file_path+s3_file_name+local_file_extension
+		local_file_name=local_file_path+file_name+local_file_extension
 		#print(local_file_name)
 
 		#- Finally, download the file
-		obj = s3_object.Object(bucket_name, s3_file_name).download_file(local_file_name)
+		obj = resource_object.Object(bucket_name, file_name).download_file(local_file_name)
 
 #- And we are done!
